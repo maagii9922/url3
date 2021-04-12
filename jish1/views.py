2c4add7c161f750553bc0b51a6561b891ccca8ae
mandah <mandakh@bumanit.mn>
2021-04-12 13:52:16 +0800

    kkkk

--- jish1/views.py
+++ jish1/views.py
@@ -1,5 +1,13 @@
 
 from django.http import HttpResponse
+from django.views import generic
 
 def home(request):
     return HttpResponse("hello")
+
+class Botview(generic.View):
+    def get(self,request, *args,**kwargs):
+        print(request)
+        return HttpResponse("req " + request)
+        
+ 
