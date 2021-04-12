72ee51079b33fdb777dcb850284e4a7b9f35a188
mandah <mandakh@bumanit.mn>
2021-04-12 13:17:28 +0800

    kkkk

---
+++ jish1/settings.py
@@ -0,0 +1,8 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4zjwtn+)h3wk31^kj_2q5jyfndm@&xwx^06ihu1!&zb_y4ybn0'
+
+DEBUG = True
+
+ROOT_URLCONF = 'jish1.urls'
+
+ALLOWED_HOSTS = []
