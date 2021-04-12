853f9512d88282af09246a8c6c2bca09d69eb0c2
mandah <mandakh@bumanit.mn>
2021-04-12 18:19:15 +0800

    kkkk

--- jish1/views.py
+++ jish1/views.py
@@ -90,6 +90,31 @@ def handleMessage(sender_psid, received_message):
     elif "attachments" in received_message :  
         attachment_url = received_message['attachments'][0]['payload']['url']
         print(attachment_url)
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Is this the right picture?",
+                    "subtitle": "Tap a button to answer.",
+                    "image_url": attachment_url,
+                    "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Yes!",
+                        "payload": "yes",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "No!",
+                        "payload": "no",
+                    }
+                    ],
+                }]
+                }
+            }
+            }
   
     
     callSendAPI(sender_psid, response)
