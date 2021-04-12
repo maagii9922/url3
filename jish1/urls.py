5b42bc733ebb3db42af1ad786247b4c6335cab0b
mandah <mandakh@bumanit.mn>
2021-04-12 13:54:45 +0800

    kkkk

--- jish1/urls.py
+++ jish1/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import home,Botview
 
 urlpatterns = [
-    path('',home)
+    path('',home),
+    path('webhook',Botview.as_view())
 ]
