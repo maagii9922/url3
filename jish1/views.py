659342fb8deef918a0ddf979a6c66182f4638454
mandah <mandakh@bumanit.mn>
2021-04-12 14:30:24 +0800

    kkkk

--- jish1/views.py
+++ jish1/views.py
@@ -1,10 +1,14 @@
 
 from django.http import HttpResponse
 from django.views import generic
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return HttpResponse("hello")
+
 VERIFY_TOKEN='123456'
+
 class Botview(generic.View):
     def get(self,request, *args,**kwargs):
         if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
@@ -13,4 +17,11 @@ class Botview(generic.View):
             return HttpResponse('Error, invalid token')
 
         
- 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+
+    def post(self,request,*args,**kwargs):
+        incoming_massege=json.loads(self.request.body.decode('utf-8'))
+        # for entry in incoming_massege()
+        return HttpResponse(incoming_massege)
