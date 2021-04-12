cea02284784390aeb81025a2f594014011da6dfe
mandah <mandakh@bumanit.mn>
2021-04-12 17:26:44 +0800

    kkkk

--- jish1/views.py
+++ jish1/views.py
@@ -6,11 +6,13 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import re
 import random
+import requests
 
 
 def home(request):
     return HttpResponse("hello")
 
+PAGE_ACCESS_TOKEN = "EAANZA3YNaNMEBAIW4FYwzrnZA9C3NFwZCwXDaGQUUp6wRes2d0p6HJ6ygwLd8HT5ZC04fV3zMGrF9jp3YTvJMjZCvFHaGgZCcPdV27Dvx06qulupKvZBOQCbsBqX7sRYeyabf5K1BOJV6pKUAmCMfcrZCncNG6Ry3R8JIxaKnz3GNgZDZD"
 VERIFY_TOKEN='123456'
 
 class Botview(generic.View):
@@ -34,7 +36,7 @@ class Botview(generic.View):
                 # print(m)
                 if 'message' in m:
                     post_facebook_message(m['sender']['id'],m['message']['text'])
-        return HttpResponse("okkkk")
+        return HttpResponse()
 
 
 jokes = {
@@ -43,21 +45,34 @@ jokes = {
     u'эхлэх': [u"""эхлэх-1""", u"""эхлэх-2."""]
 }
 
+
+
 def post_facebook_message(fbid, recevied_message):
     tokens = re.sub(r"[^a-zA-Z0-9А-яӨҮөү,\s]", '', recevied_message).lower()
-    print(tokens)
     joke_text = ''
 
     for key, value in jokes.items(): 
-        print(key)
         if tokens.find(key) >= 0:
             joke_text = random.choice(jokes[key])
     
     if not joke_text:
-        print(u"Би ойлгосонгүй! Бидэн уруу '%s', 'Түгээмэл асуулт хариулт', 'Эхлэх' гэж илгээнэ үү!"%("Холбоо барих"))
+        joke_text = "Би ойлгосонгүй! Бидэн уруу 'Холбоо барих', 'Түгээмэл асуулт хариулт', 'Эхлэх' гэж илгээнэ үү!"
     
-    print(joke_text)
 
+    user_details_url = "https://graph.facebook.com/v2.6/%s" % fbid
+    user_details_params = {
+        'fields': 'first_name,last_name', 'access_token': PAGE_ACCESS_TOKEN}
+    user_details = requests.get(user_details_url, user_details_params).json()
+    joke_text = 'Yo ' + user_details['first_name'] + '..! ' + joke_text
+
+
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
+    response_msg = json.dumps(
+        {"recipient": {"id": fbid}, "message": {"text": joke_text}})
+    status = requests.post(post_message_url, headers={
+                           "Content-Type": "application/json"}, data=response_msg)
+
+    return HttpResponse(joke_text)
 
 
 
