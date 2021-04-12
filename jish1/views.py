2e1722844a08edbd8e4b43d9a9647d5b65ba9746
mandah <mandakh@bumanit.mn>
2021-04-12 14:16:31 +0800

    kkkk

--- jish1/views.py
+++ jish1/views.py
@@ -4,10 +4,13 @@ from django.views import generic
 
 def home(request):
     return HttpResponse("hello")
-
+VERIFY_TOKEN='123456'
 class Botview(generic.View):
     def get(self,request, *args,**kwargs):
-        print(request)
-        return HttpResponse("req " + str(request))
+        if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
+            return HttpResponse('Error, invalid token')
+
         
  
